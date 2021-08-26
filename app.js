96763e624ab326e3ad3bbd7abd797a02b438e75a
Rishad Mohammed <rishadmhd1414@gmail.com>
2021-08-26 19:27:10 +0530

    Database connected

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
 var hbs=require('express-handlebars')
 
 var app = express();
+var fileUpload=require('express-fileupload')
+var db=require('./config/connection')
 
 
 // view engine setup
@@ -21,9 +23,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload())
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+db.connect((err)=>{
+  if(err)
+  console.log('Connection Error'+err)
+  else
+  console.log('Database connected to port 27017 successfuly')
+})
+
+app.use('/', userRouter);
+app.use('/admin', adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
