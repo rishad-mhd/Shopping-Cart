d09be4b3759fa6600c2d866a6b5579cd0835bb52
Rishad Mohammed <rishadmhd1414@gmail.com>
2021-08-26 23:00:58 +0530

    Bring products from database

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,10 +1,17 @@
 var db=require('../config/connection')
+var collection=require('../config/colllections')
 module.exports={
     addProduct:(product,callback)=>{
         console.log(product)
-        db.get().collection('product').insertOne(product).then((data)=>{
+        db.get().collection(collection.PRODUCT_COLLECTION).insertOne(product).then((data)=>{
             console.log(data)
             callback(data.insertedId);
+        })
+    },
+    getAllProducts:()=>{
+        return new Promise(async(resolve,reject)=>{
+            let products=await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray()
+            resolve(products)
         })
     }
 }
