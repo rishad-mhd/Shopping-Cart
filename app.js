daa277c409ab01f11a08d628b1ba911bf93683b3
Rishad Mohammed <rishadmhd1414@gmail.com>
2021-11-20 17:32:13 +0530

    Style changed

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
-app.use(session({secret:"Key",cookie:{maxAge:600000}}))
+app.use(session({secret:"Key",cookie:{maxAge:24*60*60*1000}}))
 
 db.connect((err)=>{
   if(err)
